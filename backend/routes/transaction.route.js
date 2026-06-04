19bb9596d4d914ae75a78533d32bf02c5bdaa425
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2026-06-04 21:44:01 +0530

    Added the delete route

--- backend/routes/transaction.route.js
+++ backend/routes/transaction.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createTransaction, getTransactions } from '../controllers/transaction.controller.js';
+import { createTransaction, getTransactions, deleteTransaction } from '../controllers/transaction.controller.js';
 import { requireAuth } from '@clerk/express';
 
 const router = express.Router();
 
 router.post("/", requireAuth(), createTransaction);
 router.get("/", requireAuth(), getTransactions);
+router.delete('/:id', requireAuth(), deleteTransaction);
 
 export default router;
