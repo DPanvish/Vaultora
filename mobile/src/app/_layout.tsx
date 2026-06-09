389f4fbe09cb9e37fc01a22a4cd046edaad490e4
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2026-06-09 22:09:47 +0530

    Auth setup done and deleted some unnecesary files

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,17 +1,29 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
+import { tokenCache } from '../utils/tokenCache';
 
 const queryClient = new QueryClient();
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+    throw new Error('Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY to your .env file');
+}
+
 const RootLayout = () => {
     return (
-        <QueryClientProvider client={queryClient}>
-            <StatusBar style="light" />
-            <Stack screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="index" />
-            </Stack>
-        </QueryClientProvider>
+        <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+            <ClerkLoaded>
+                <QueryClientProvider client={queryClient}>
+                    <StatusBar style="light" />
+                    <Stack screenOptions={{ headerShown: false }}>
+                        <Stack.Screen name="index" />
+                    </Stack>
+                </QueryClientProvider>
+            </ClerkLoaded>
+        </ClerkProvider>
     )
 }
 
